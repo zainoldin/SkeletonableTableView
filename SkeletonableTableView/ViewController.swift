49fc4a4a52e4b54f195f0d9bcd4125235a1ec837
zainoldin <zainoldinalmas@gmail.com>
2020-07-05 22:02:49 +0600

    feat: add project

---
+++ SkeletonableTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkeletonableTableView
+//
+//  Created by Almas Zainoldin on 7/5/20.
+//  Copyright © 2020 crococoders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
